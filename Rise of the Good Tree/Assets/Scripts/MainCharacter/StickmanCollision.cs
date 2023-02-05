9df0fc838c90b1c8c1bcae0f07f5fb71990b55f9
Nyursultan <nyurguntarskii@gmail.com>
2023-02-05 11:50:09 +0900

    scene and fish animation added

--- Rise of the Good Tree/Assets/Scripts/MainCharacter/StickmanCollision.cs
+++ Rise of the Good Tree/Assets/Scripts/MainCharacter/StickmanCollision.cs
@@ -10,5 +10,13 @@ public class StickmanCollision : MonoBehaviour
         {
             FindObjectOfType<AudioManager>().Play("Step");
         }
+        if (collision.collider.tag == "PlatformBridge" && Input.GetAxisRaw("Horizontal") != 0)
+        {
+            FindObjectOfType<AudioManager>().Play("FootstepWood");
+        }
+        if (collision.collider.tag == "PlatformStone" && Input.GetAxisRaw("Horizontal") != 0)
+        {
+            FindObjectOfType<AudioManager>().Play("FootstepStone");
+        }
     }
 }
